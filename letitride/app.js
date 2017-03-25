a57fce78c458cab97c6020597a583415b407fb8b
Clevercav <casper.lindinger@gmail.com>
2017-03-24 22:25:29 -0700

    asdqw

--- letitride/app.js
+++ letitride/app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var firebase = require('firebase');
 
 var mongoose = require('mongoose');
 require('./models/Users');
@@ -13,7 +14,7 @@ mongoose.connect('mongodb://localhost/letitride');
 var passport = require('passport');
 
 var index = require('./routes/index');
-var users = require('./routes/users'); 
+var users = require('./routes/users');
 
 var app = express();
 
@@ -25,7 +26,9 @@ app.set('view engine', 'ejs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize());
@@ -33,14 +36,14 @@ app.use('/', index);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
