6dbbc5679d7755ae04a702362472991ffbaa6dcd
Clevercav <casper.lindinger@gmail.com>
2017-04-04 14:39:47 -0700

    Added firebase (not working yet though) Fixed contact/about views.

---
+++ letitride/public/javascripts/database.js
@@ -0,0 +1,11 @@
+var firebase = require("firebase");
+
+var config = {
+    apiKey: "AIzaSyDfzjShETigboXiRAVJHXBWgkFntyiynQ0",
+    authDomain: "let-it-ride-1488500810105.firebaseapp.com",
+    databaseURL: "https://let-it-ride-1488500810105.firebaseio.com",
+    projectId: "let-it-ride-1488500810105",
+    storageBucket: "let-it-ride-1488500810105.appspot.com",
+    messagingSenderId: "545902173984"
+};
+firebase.initializeApp(config);
